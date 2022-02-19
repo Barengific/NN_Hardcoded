c2d64b4b16458c6cc47b7dd25424e8e165a5e5f3
Barengific <75044077+Barengific@users.noreply.github.com>
2022-02-19 20:22:08 +0000

    Update Ex02.py

--- Ex02.py
+++ Ex02.py
@@ -5,7 +5,7 @@
 
 import numpy as np
 
-#single neuron
+#single neuron + numpy
 ins = [1.0, 2.0, 3.0, 2.5]
 ws = [0.2, 0.8, -0.5, 1.0]
 bias = 2.0
@@ -13,23 +13,20 @@ outs = np.dot(ws, ins) + bias
 #print(outs)
 
 
-#layer of neurons
+#single layer of neurons with numpy
 ws = [[0.2, 0.8, -0.5, 1],
       [0.5, -0.91, 0.26, -0.5],
       [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
-layer_outs = np.dot(ws, ins) + biases
-print(layer_outs)
+outs = np.dot(ws, ins) + biases
+# print(outs)
 
 
 ins = [[1.0, 2.0, 3.0, 2.5],
        [2.0, 5.0, -1.0, 2.0],
        [-1.5, 2.7, 3.3, -0.8]]
-ws = [[0.2, 0.8, -0.5, 1.0],
-      [0.5, -0.91, 0.26, -0.5],
-      [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
-layer_outs = np.dot(ins, np.array(ws).T) + biases
-print(layer_outs)
+outs = np.dot(ins, np.array(ws).T) + biases
+print(outs)
 
 
